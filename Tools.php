baf0b48bf69ba58a7b3f91cb6f57b4a4e84d3de2
t yy <649909457@qq.com>
2020-03-10 18:41:39 +0800

    获取汉子拼音首字母

--- Tools.php
+++ Tools.php
@@ -65,10 +65,10 @@ class Tools extends \yii\helpers\ArrayHelper {
 	public static function format_number($number, $type = 1) {
 		switch ($type) {
 		case 1: //两位小数
-			return sprintf("%.2f", $number);
+			return sprintf('%.2f', $number);
 			break;
 		case 2: //手机号码
-			return substr($number, 0, 5) . "****" . substr($number, 9, 2);
+			return substr($number, 0, 5) . '****' . substr($number, 9, 2);
 			break;
 
 		default:
@@ -83,7 +83,7 @@ class Tools extends \yii\helpers\ArrayHelper {
 	 * @return bool
 	 */
 	public static function is_mobile($mobile = '') {
-		if (preg_match("/^1[3456789]{1}\d{9}$/", $mobile)) {
+		if (preg_match('/^1[3456789]{1}\d{9}$/', $mobile)) {
 			return true;
 		} else {
 			return false;
@@ -275,7 +275,7 @@ class Tools extends \yii\helpers\ArrayHelper {
 	 * @param  string $eof  结束标记
 	 * @return [type]       [description]
 	 */
-	public static function srequest(string $host, string $cmd, $data, $ext = [], $eof = "\r\n\r\n") {
+	public static function srequest(string $host, string $cmd, $data, $ext = [], $eof = '\r\n\r\n') {
 		$fp = stream_socket_client($host, $errno, $errstr);
 		if (!$fp) {
 			throw new Exception("stream_socket_client fail errno={$errno} errstr={$errstr}");
@@ -453,7 +453,7 @@ class Tools extends \yii\helpers\ArrayHelper {
 			$ip = $_SERVER['REMOTE_ADDR'];
 		}
 		// IP地址合法验证
-		$long = sprintf("%u", ip2long($ip));
+		$long = sprintf('%u', ip2long($ip));
 		$ip = $long ? array($ip, $long) : array('0.0.0.0', 0);
 		return $ip[$type];
 	}
@@ -478,5 +478,60 @@ class Tools extends \yii\helpers\ArrayHelper {
 	        return true;
 	    }  
         return false;
+	}
+
+	/**
+	 * 汉子拼音首字母
+	 * @param  [type] $s0 [description]
+	 * @return [type]     [description]
+	 */
+	public static function zhFirstChar($str){
+        if (empty($str)) {
+            return '';
+        }
+        $fchar = ord($str{0});
+        if ($fchar >= ord('A') && $fchar <= ord('z')) return strtoupper($str{0});
+        $s1 = iconv('UTF-8', 'gb2312', $str);
+        $s2 = iconv('gb2312', 'UTF-8', $s1);
+        $s = $s2 == $str ? $s1 : $str;
+        $asc = ord($s{0}) * 256 + ord($s{1}) - 65536;
+
+	    if($asc >= -20319 and $asc <= -20284) return 'A';
+	    if($asc >= -20283 and $asc <= -19776) return 'B';
+	    if($asc >= -19775 and $asc <= -19219) return 'C';
+	    if($asc >= -19218 and $asc <= -18711) return 'D';
+	    if($asc >= -18710 and $asc <= -18527) return 'E';
+	    if($asc >= -18526 and $asc <= -18240) return 'F';
+	    if($asc >= -18239 and $asc <= -17923) return 'G';
+	    if($asc >= -17922 and $asc <= -17418) return 'I';
+	    if($asc >= -17417 and $asc <= -16475) return 'J';
+	    if($asc >= -16474 and $asc <= -16213) return 'K';
+	    if($asc >= -16212 and $asc <= -15641) return 'L';
+	    if($asc >= -15640 and $asc <= -15166) return 'M';
+	    if($asc >= -15165 and $asc <= -14923) return 'N';
+	    if($asc >= -14922 and $asc <= -14915) return 'O';
+	    if($asc >= -14914 and $asc <= -14631) return 'P';
+	    if($asc >= -14630 and $asc <= -14150) return 'Q';
+	    if($asc >= -14149 and $asc <= -14091) return 'R';
+	    if($asc >= -14090 and $asc <= -13319) return 'S';
+	    if($asc >= -13318 and $asc <= -12839) return 'T';
+	    if($asc >= -12838 and $asc <= -12557) return 'W';
+	    if($asc >= -12556 and $asc <= -11848) return 'X';
+	    if($asc >= -11847 and $asc <= -11056) return 'Y';
+	    if($asc >= -11055 and $asc <= -10247) return 'Z';
+	    return null;
+	}
+
+	/**
+	 * 中文字符首字母
+	 * @param  [type] $zh [description]
+	 * @return [type]     [description]
+	 */
+	public static function zhPinYin($zh){
+	    $ret = '';
+	    for($i = 0; $i < strlen($zh); $i++){
+            $ret .= static::zhFirstChar(mb_substr($zh,$i,1));
+	    }
+	    return strtolower($ret);
 	}
 }
